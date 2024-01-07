4d9b335c0c2216713591ea699504f9e2f31b2968
Christian CoC <christianelmillo81@gmail.com>
2024-01-06 23:19:22 -0300

    actualizado para eliminar de la pc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModelsComponent } from './models/models.component';
+import { PagesComponent } from './pages/pages.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { DetailServiceComponent } from './pages/detail-service/detail-service.component';
+import { ApiServiceComponent } from './api-service/api-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelsComponent,
+    PagesComponent,
+    ContactComponent,
+    HomeComponent,
+    ServiceComponent,
+    DetailServiceComponent,
+    ApiServiceComponent
   ],
   imports: [
     BrowserModule,
